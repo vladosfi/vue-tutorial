db95a1285ae5227b275ec9d6dd20d062dc17004f
Vladimir Ivanov <vgivanov@gmail.com>
2023-02-21 21:41:34 +0200

    Lifecycle Hooks

---
+++ lifecycle-hooks/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <!-- Lifecycle Hooks -->
+  <ParentComponent />
+</template>
+
+<script>
+import ParentComponent from './components/ParentComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ParentComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ lifecycle-hooks/src/components/ChildComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>Child Component</div>
+</template>
+
+<script>
+export default {
+  name: "ChildComponent",
+  beforeCreate() {
+    console.log("Child beforeCreate()");
+  },
+  created() {
+    console.log("Child created()");
+  },
+  beforeMount() {
+    console.log("Child beforeMount()");
+  },
+  mounted() {
+    console.log("Child mounted()");
+  },
+  beforeUpdate() {
+    console.log("Child beforeUpdate()");
+  },
+  updated() {
+    console.log("Child updated()");
+  },
+  beforeUnmount() {
+    console.log("Child beforeUnmount()");
+  },
+  unmounted() {
+    console.log("Child unmounted()");
+  },
+};
+</script>
+
+<style scoped></style>
