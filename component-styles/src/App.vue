873770afe24544cfe1b09959e95a99de152ab09e
vladosfi <vladosfi@gmail.com>
2023-02-21 15:17:23 +0200

    Component Styles

---
+++ component-styles/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <h4>App component text</h4>
+
+
+  <ChildStyles><h4>For ChildStyles in slots is applied css from parent component</h4></ChildStyles>
+</template>
+
+<script>
+import ChildStyles from './components/ChildStyles.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ChildStyles
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h4{
+  color: orange;
+}
+</style>
---
+++ component-styles/src/components/ChildStyles.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h4>ChildStyles component text using SCOPED</h4>
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'ChildStyles'
+  }
+</script>
+
+<style scoped>
+h4{
+  color: red;
+}
+</style>
