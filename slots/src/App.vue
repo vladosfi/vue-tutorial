4001fb6c5c8df2755150e2814359014923d05e26
vladosfi <vladosfi@gmail.com>
2023-02-21 14:43:15 +0200

    Slots

---
+++ slots/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <CardSlot></CardSlot>
+
+  <CardSlot>Card content</CardSlot>
+  <CardSlot><h2>Card content</h2></CardSlot>
+  <CardSlot><h2>Card img</h2></CardSlot>
+
+  <NamedSlots>
+    <template v-slot:header>
+      <h3>HEader</h3>
+    </template>
+
+    <template v-slot:default>
+      <h3>Image src</h3>
+    </template>
+
+    <template v-slot:footer>
+      <button>View Details</button>
+    </template>
+  </NamedSlots>
+
+  //Slots Props
+  <SlotsProps>
+    <template v-slot:default="slotProps"> {{ slotProps.firstName }} {{ slotProps.lastName }} </template>
+  </SlotsProps>
+  <SlotsProps>
+    <template v-slot:default="slotProps"> {{ slotProps.lastName }} {{ slotProps.firstName }} </template>
+  </SlotsProps>
+  <SlotsProps>
+    <template v-slot:default="slotProps">
+      {{ slotProps.firstName }}
+    </template>
+  </SlotsProps>
+</template>
+
+<script>
+import CardSlot from "./components/CardSlot.vue";
+import NamedSlots from "./components/NamedSlots.vue";
+import SlotsProps from "./components/SlotsProps.vue";
+
+export default {
+  name: "App",
+  components: {
+    CardSlot,
+    NamedSlots,
+    SlotsProps,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ slots/src/components/SlotsProps.vue
@@ -0,0 +1,31 @@
+<template>
+  <h3 v-for="name in names" :key="name.firstName">
+    <slot :firstName="name.firstName" :lastName="name.lastName"></slot>
+  </h3>
+</template>
+
+<script>
+export default {
+  name: "SlotsProps",
+  data() {
+    return {
+      names: [
+        {
+          firstName: "Ivan",
+          lastName: "Draganov",
+        },
+        {
+          firstName: "Al",
+          lastName: "Bundy",
+        },
+        {
+          firstName: "Open",
+          lastName: "AI",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
