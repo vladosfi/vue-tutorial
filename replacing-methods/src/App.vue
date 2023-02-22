fee7cbad6177da8a47e14fa038917c1c554b1830
vladosfi <vladosfi@gmail.com>
2023-02-22 15:35:30 +0200

    Replacing methods

---
+++ replacing-methods/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <MethodsReplace/>
+</template>
+
+<script>
+import MethodsReplace from './components/MethodsReplace.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MethodsReplace
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ replacing-methods/src/components/MethodsReplace.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+    <button @click="setName">Change Name</button>
+
+    <h2>{{ count }}</h2>
+    <button @click="incrementCount">Increment Count</button>
+
+    <h2>{{ first }} {{ last }}</h2>
+    <button @click="changeHero">Change Hero</button>
+  </div>
+</template>
+
+<script>
+import { ref, reactive, toRefs } from "vue";
+
+export default {
+  name: "MethodsReplace",
+  setup() {
+    const count = ref(0);
+    const state = reactive({
+      first: "Bruce",
+      last: "Wayne",
+    });
+
+    function incrementCount() {
+      count.value++;
+    }
+
+    function changeHero() {
+      state.first = "Clark";
+      state.last = "Kent";
+    }
+
+    return {
+      count,
+      incrementCount,
+      changeHero,
+      ...toRefs(state),
+    };
+  },
+  data() {
+    return {
+      name: "Vladimir",
+    };
+  },
+  methods: {
+    setName() {
+      this.name = "Coede";
+    },
+  },
+};
+</script>
+
+<style scoped></style>
